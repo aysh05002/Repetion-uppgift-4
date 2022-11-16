25c13ec74d69ea2958e54e0efb98cabf48a42842
Admin <Admin@TEB123008>
2022-11-16 10:32:05 +0100

    Add project files.

---
+++ Repetion uppgift 4/Repetion_uppgift_4.py
@@ -0,0 +1,16 @@
+def information():
+    print("\n\t\t\t\t\t\t----------------------")
+    print("\t\t\t\t\t\t| Rubrik: Uppgift 4  |")
+    print("\t\t\t\t\t\t|                    |")
+    print("\t\t\t\t\t\t| Namn: Ayman Shakra |")
+    print("\t\t\t\t\t\t|                    |")
+    print("\t\t\t\t\t\t| Datum: 2022/11/16  |")
+    print("\t\t\t\t\t\t----------------------\n")
+
+information()
+
+print("\t\t\t\t\t Du kommer att skriva tva olika tal, sen \n\t\t\t\t\t kommer du fa fram medelvardet av dom\n")
+tal1=float(input("\t\t\t\t\t\t Tal 1: "))
+tal2=float(input("\t\t\t\t\t\t Tal 2: "))
+medelvardet=(tal1+tal2)/2
+print("\t\t\t\t\t\t Medelvardet ar", medelvardet)
